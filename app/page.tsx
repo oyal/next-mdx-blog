e382cf8e9159b15836a5929348d4b2baef62fae1
oyal <oyal616@qq.com>
2023-12-22 15:50:19 +0800

    style: 修改了主页样式

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,13 @@ export default async function Home() {
     <div className="space-y-8 py-4">
       {posts.map((post, index) => (
         <div key={index}>
-          <h2 className="text-xl font-bold">
+          <h2 className="text-xl font-bold text-emerald-500 hover:text-emerald-600 dark:text-emerald-400 dark:hover:text-emerald-500">
             <Link href={`/blogs/${post.slug}`}>{post.title}</Link>
           </h2>
-          <p>{post.description}</p>
-          <time className="my-1 block text-sm text-zinc-500">
+          <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+            {post.description}
+          </p>
+          <time className="my-1 block text-xs text-zinc-400 dark:text-zinc-500">
             {format(post.date, 'yyyy-MM-dd')}
           </time>
         </div>
