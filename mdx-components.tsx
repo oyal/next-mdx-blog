952bc04f995c36f13d0ca11f19652688743a9d5b
oyal <oyal616@qq.com>
2024-05-17 11:09:06 +0800

    refactor: 移除h2锚点跳转

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,7 @@
 import type { MDXComponents } from 'mdx/types'
-import * as mdxComponents from '@/components/mdx'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    ...mdxComponents,
   }
 }
--- mdx/rehype.mjs
+++ mdx/rehype.mjs
@@ -1,18 +1,4 @@
 import rehypeHighlight from 'rehype-highlight'
-import { visit } from 'unist-util-visit'
-import { slugifyWithCounter } from '@sindresorhus/slugify'
-import { toString } from 'mdast-util-to-string'
-
-function rehypeSlugify() {
-  return (tree) => {
-    let slugify = slugifyWithCounter()
-    visit(tree, 'element', (node) => {
-      if (node.tagName === 'h2' && !node.properties.id) {
-        node.properties.id = slugify(toString(node))
-      }
-    })
-  }
-}
 
 export const rehypePlugins = [
   [
@@ -21,5 +7,4 @@ export const rehypePlugins = [
       detect: true,
     },
   ],
-  rehypeSlugify,
 ]
