c29332acc4e354ef8e7f1d9e719e96f6d8c3e572
oyal <oyal616@qq.com>
2024-07-28 23:57:30 +0800

    feat: 计数动画

---
+++ code-snippet/framer-motion/counter.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from 'react'
+import { useInView, useMotionValue, useSpring } from 'framer-motion'
+
+const Counter = () => {
+  const ref = useRef<HTMLSpanElement>(null)
+  const motionValue = useMotionValue(0)
+  const springValue = useSpring(motionValue, {
+    damping: 100,
+    stiffness: 100,
+  })
+
+  const isInView = useInView(ref, {
+    once: true,
+  })
+
+  useEffect(() => {
+    if (isInView) {
+      console.log('123')
+      motionValue.set(123123)
+    }
+  }, [motionValue, isInView])
+
+  useEffect(
+    () =>
+      springValue.on('change', (latest) => {
+        if (ref.current) {
+          ref.current.textContent = Intl.NumberFormat('en-US').format(
+            +latest.toFixed(0),
+          )
+        }
+      }),
+    [springValue],
+  )
+
+  return <span ref={ref} className="text-2xl font-bold" />
+}
+
+export default Counter
